e2eadcacbfe9d199d2a03f1b1ab536148cb85483
kennysvensson1 <40225691+kennysvensson1@users.noreply.github.com>
2020-03-04 18:58:28 +0100

    Blips
    Blips finns nu med. Enkel config om du vill ha det eller ej.

--- arp_voi/config.lua
+++ arp_voi/config.lua
@@ -1,16 +1,21 @@
-Config                            = {}
+Config = {}
 
 --- #### BASICS
 Config.EnablePrice = false -- false = bikes for free
+Config.EnableBlips = true
 
 
 --- #### PRICES	
-Config.Price = 50
-Config.PPM = 10 -- Price Per Minute
-Config.VoiModel = `serv_electricscooter`
+Config.Price = 60
+Config.PPM = 30 -- Price Per Minute
+Config.VoiModel = 'serv_electricscooter'
 
 Config.Locations = {
     vector3(306.14, -235.24, 54.1),
     vector3(218.76, -868.51, 30.49),
     vector3(-1037.87, -2737.51, 20.17),
+    vector3(-1379.61, -584.32, 30.09),
+    vector3(-356.42, -121.96, 38.7),
+    vector3(1853.78, 2581.88, 45.67),
+    vector3(293.16, -615.73, 43.43),
 }
